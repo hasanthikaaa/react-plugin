2c0849ab06874235f608877c059e1edd7599dc7d
Hasanthika <hasanthika.gamage96@gmail.com>
2024-10-01 15:18:23 +0530

    c

--- src/__test__/components/MyButton.test.tsx
+++ src/__test__/components/MyButton.test.tsx
@@ -5,8 +5,6 @@ import { MyButton } from '../../index';
 describe('MyButton Component', () => {
   it('should render the component without crashing', () => {
     const { getByText } = render(<MyButton />);
-    expect(getByText('This is the update!')).toBeInTheDocument();
-    expect(getByText('Latest')).toBeInTheDocument();
-    expect(getByText('Hello World')).toBeInTheDocument();
+    expect(getByText('Hi')).toBeInTheDocument();
   });
 });
